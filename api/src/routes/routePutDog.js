941fba5c385fbc2e723df0a8aff70f67c356667b
Julian Gomez <juliangomez.xvii@gmail.com>
2022-08-29 17:10:26 -0300

    route edit dog completed

--- api/src/routes/routePutDog.js
+++ api/src/routes/routePutDog.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
 const router = Router()
 const { Dog, Temperament} = require('../db.js')
+const { Op } = require("sequelize");
 
 router.put('/:id', async (req, res) => {
   const {id} = req.params
@@ -11,26 +12,75 @@ router.put('/:id', async (req, res) => {
   try {
 
     const dogsEdit = await Dog.findByPk(id)
-    const oldTemperament = temperament
 
     if(!dogsEdit) {
       res.status(404).send('No se encontró el id')
     }
 
-    if(name){condition.name = name}
-    if(heightMin){condition.heightMin = heightMin}
-    if(heightMax){condition.heightMax = heightMax}
-    if(weightMin){condition.weightMin = weightMin}
-    if(weightMax){condition.weightMax = weightMax}
-    if(lifeSpan){condition.lifeSpan = lifeSpan}
-    if(image){condition.image = image}
-    let temperamentDb = await Temperament.findAll({
-    where: { name: temperament }
-    })
+    if(name){
+      condition.name = name
+    }
+    else {
+      condition.name = ''
+    }
 
+    if(heightMin){
+      condition.heightMin = heightMin
+    }
+    else {
+      condition.heightMin = ''
+    }
+
+    if(heightMax){
+      condition.heightMax = heightMax
+    }
+    else {
+      condition.heightMax = ''
+    }
+    
+    if(weightMin){
+      condition.weightMin = weightMin
+    }
+    else {
+      condition.weightMin = ''
+    }
+
+    if(weightMax){
+      condition.weightMax = weightMax
+    }
+    else {
+      condition.weightMax = ''
+    }
+
+    if(lifeSpan){
+      condition.lifeSpan = lifeSpan
+    }
+    else {
+      condition.lifeSpan = ''
+    }
+
+    if(image){
+      condition.image = image
+    }
+    else {
+      condition.image = ''
+    }
+
+    if (temperament) {
+      let temperamentDelete = await Temperament.findAll({
+        where: { name: { [Op.notLike]: `${temperament}` } },
+      });
+      let temperamentDb = await Temperament.findAll({
+        where: { name: temperament },
+      });
+      console.log(temperamentDelete)
+      console.log(temperamentDb)
+
+      await dogsEdit.removeTemperament(temperamentDelete);
+      await dogsEdit.addTemperament(temperamentDb);
+    }
 
     await dogsEdit.update(condition)
-    await dogsEdit.addTemperament(temperamentDb)
     res.send(`La raza fue modificada con exito`)
   }
   catch (error) {
