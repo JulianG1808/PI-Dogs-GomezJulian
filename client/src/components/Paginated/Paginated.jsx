3443264d981fcf8b9dbd81351857e6e31904df97
Julian Gomez <juliangomez.xvii@gmail.com>
2022-09-09 18:35:57 -0300

    rename classnames and add li tag

--- client/src/components/Paginated/Paginated.jsx
+++ client/src/components/Paginated/Paginated.jsx
@@ -15,17 +15,17 @@ export default function Paginated ({dogsPerPage, allDogs, paginated, currentPage
     return (
         <nav> 
             <ul className='pagination'>
-                <li><button 
-                 className="inactive" 
+                <button 
+                 className="functions arrowPrev" 
                  onClick={() => paginated(currentPage - 1)} 
                  disabled={currentPage === 1}>
                     <GrIcons.GrPrevious />
-                </button></li>
+                </button>
                 <li><button className={currentPage === 1 ? 'active' : 'inactive'}onClick={() => paginated(1)}>1</button></li>
                 {currentPage > 5 ? 
-                    <button className="inactive" onClick={() => paginated(currentPage-4)}>
-                        ...
-                    </button> 
+                    <li><button className="functions more" onClick={() => paginated(currentPage-4)}>
+                        <GrIcons.GrMore />
+                    </button></li> 
                     : null
                 }
                 {
@@ -46,19 +46,19 @@ export default function Paginated ({dogsPerPage, allDogs, paginated, currentPage
                     ).slice(currentPage-1, currentPage+4)
                 }
                 {currentPage+2 < Math.ceil(allDogs / dogsPerPage) - 3 ?
-                    <button className="inactive" onClick={() => paginated(currentPage+5)}>
-                        ...
-                    </button> 
+                    <li><button className="functions more" onClick={() => paginated(currentPage+5)}>
+                        <GrIcons.GrMore />
+                    </button></li> 
                     : null
                 }
-                <button 
+                <li><button 
                  className={Math.ceil(currentPage) !== Math.ceil(allDogs / dogsPerPage) ? 'inactive' : 'active'} 
                  onClick={() => paginated(allDogs / dogsPerPage)}
                 >
                     {Math.ceil(allDogs / dogsPerPage)}
-                </button>
+                </button></li>
                 <button 
-                 className="inactive" 
+                 className="functions arrowNext" 
                  onClick={() => paginated(currentPage + 1)} 
                  disabled={Math.ceil(currentPage) === Math.ceil(allDogs / dogsPerPage)}><GrIcons.GrNext /></button>
             </ul>
