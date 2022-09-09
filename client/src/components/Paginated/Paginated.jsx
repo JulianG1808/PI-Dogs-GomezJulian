6dc91f041fa1bf5fe93c8f2254cc9f0caedfb1f0
Julian Gomez <juliangomez.xvii@gmail.com>
2022-09-09 18:17:56 -0300

    optimize render (incomplete)

--- client/src/components/Paginated/Paginated.jsx
+++ client/src/components/Paginated/Paginated.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 //import css
 import './Paginated.css'
+import * as GrIcons from "react-icons/gr";
 
 //-------------------------------------------------------------------------------------------------------------
 export default function Paginated ({dogsPerPage, allDogs, paginated, currentPage}){
@@ -14,17 +15,52 @@ export default function Paginated ({dogsPerPage, allDogs, paginated, currentPage
     return (
         <nav> 
             <ul className='pagination'>
+                <li><button 
+                 className="inactive" 
+                 onClick={() => paginated(currentPage - 1)} 
+                 disabled={currentPage === 1}>
+                    <GrIcons.GrPrevious />
+                </button></li>
+                <li><button className={currentPage === 1 ? 'active' : 'inactive'}onClick={() => paginated(1)}>1</button></li>
+                {currentPage > 5 ? 
+                    <button className="inactive" onClick={() => paginated(currentPage-4)}>
+                        ...
+                    </button> 
+                    : null
+                }
                 {
                     pageNumbers?.map(number => {
                         let active = (currentPage === number)
                         return (
                         <li key={number}>
-                            <button className={active ? 'active' : 'inactive'} onClick={() => paginated(number)}>{number}</button>
+                            {number !== 1 && number !== Math.ceil(allDogs/dogsPerPage) ? 
+                                <button 
+                                 className={active ? 'active' : 'inactive'} 
+                                 onClick={() => paginated(number)}>{number}
+                                </button> 
+                                : null
+                            }
                         </li>
                         )
                     }
-                    )
+                    ).slice(currentPage-1, currentPage+4)
                 }
+                {currentPage+2 < Math.ceil(allDogs / dogsPerPage) - 3 ?
+                    <button className="inactive" onClick={() => paginated(currentPage+5)}>
+                        ...
+                    </button> 
+                    : null
+                }
+                <button 
+                 className={Math.ceil(currentPage) !== Math.ceil(allDogs / dogsPerPage) ? 'inactive' : 'active'} 
+                 onClick={() => paginated(allDogs / dogsPerPage)}
+                >
+                    {Math.ceil(allDogs / dogsPerPage)}
+                </button>
+                <button 
+                 className="inactive" 
+                 onClick={() => paginated(currentPage + 1)} 
+                 disabled={Math.ceil(currentPage) === Math.ceil(allDogs / dogsPerPage)}><GrIcons.GrNext /></button>
             </ul>
         </nav>
     )
